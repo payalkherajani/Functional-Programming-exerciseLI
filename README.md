0efb1f4ddc2d2e9fcec3b82c529d4ae235232926
payalkherajani <payal.kherajani19@gmail.com>
2021-03-14 10:37:51 +0530

    Updated-README

---
+++ README.md
@@ -0,0 +1,3 @@
+> **FunctionalProgramming-Practice-Exercises**
+---
+*Link* [exercises](https://github.com/payalkherajani/Functional-Programming-exerciseLI)
--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -3,17 +3,19 @@ import React from 'react'
 const Compose = () => {
 
     const myevalfunc = (a, c) => {
-        console.log(a, c);
-        return c
+        return c(a);
     }
 
     const mycomposefunc = (...args) => {
-        return args.reduce(myevalfunc, args[0]);
+        const func = (ip) => {
+            return args.reduce(myevalfunc, ip);
+        }
+        return func;
     }
 
     const increment = num => num + 1;
     const square = num => num * num;
-
+    // console.log(mycomposefunc(increment, square))
     return (
         <div>
             <button onClick={() => console.log(mycomposefunc(increment, square)(5))}>Compose All</button>
